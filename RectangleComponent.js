7b2d7e5b2e0827e03626818cc85321fac3225e3c
ezugudor <nelsonsmrt@gmail.com>
2019-06-18 04:00:08 +0100

    my first git commit with THis people baba baba bababooo

---
+++ RectangleComponent.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const RectangleComponent = () => (
+  <svg width={300} height={80} viewBox="0 0 300 80">
+    <defs>
+      <style>{".a{fill:#fff;}"}</style>
+    </defs>
+    <rect className="a" width={300} height={80} />
+  </svg>
+);
+
+export default RectangleComponent;
